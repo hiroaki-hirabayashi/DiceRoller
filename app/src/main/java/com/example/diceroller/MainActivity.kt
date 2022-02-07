f13304396f3b49e95811df0b59448fc2a5e8c6f9
Hiroaki Hirabayashi <hh.38063806@gmail.com>
2022-02-08 01:45:18 +0900

    Button押下でサイコロ表示

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -25,21 +25,50 @@ class MainActivity : AppCompatActivity() {
 
             rollDice()
         }
+        // アプリ起動時のサイコロ状態
+        rollDice()
     }
 
     private fun rollDice() {
-        // 初期値
+        // 初期値 6面体のサイコロオブジェクトを新規作成、サイコロを振る
         val dice = Dice(6)
+        val dice2 = Dice(6)
         // roll呼び出し
         val diceRoll = dice.roll()
+        val diceRoll2 = dice2.roll()
         // textViewのid
         // val resultTextView: TextView = findViewById(R.id.textView)
         // textViewのtextにStringにキャストした値を入れる
         // resultTextView.text = diceRoll.toString()
 
-
+        // レイアウト内のImageViewを検索する
         val diceImage: ImageView = findViewById(R.id.imageView)
-        diceImage.setImageResource(R.drawable.dice_2)
+        val diceImage2: ImageView = findViewById(R.id.imageView2)
+        // diceImage.setImageResource(R.drawable.dice_2)
+
+        // サイコロの出目から、どの描画リソースIDを使用するかを決定
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        val drawableResource2 = when (diceRoll2) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        // ImageViewを正しい描画リソースIDで更新。
+        diceImage.setImageResource(drawableResource)
+        diceImage2.setImageResource(drawableResource2)
+        diceImage.contentDescription = diceRoll.toString()
+
     }
 }
 
